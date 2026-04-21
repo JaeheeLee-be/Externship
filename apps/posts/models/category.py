fcf4ad1dd3f586c6ef3277fbadc0f5060f187c4b
RyuHawon <hfixjsj49@gmail.com>
2026-04-21 16:08:06 +0900

    refactor: PostCategory.name unique 제약 추가

--- apps/posts/models/category.py
+++ apps/posts/models/category.py
@@ -4,7 +4,7 @@ from apps.core.models import TimeStampModel
 
 
 class PostCategory(TimeStampModel):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
     status = models.BooleanField(default=True)
 
     class Meta:
