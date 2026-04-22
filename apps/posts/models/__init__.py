b0a78dca8c79e24aedadfe09d579c45160c66971
JaeheeLee-be <clodusky1@gmail.com>
2026-04-22 15:34:14 +0900

    ✨ feat: PostComment, PostCommentTag 모델 추가
    게시글 댓글(PostComment)과 댓글 멘션 태그(PostCommentTag) 모델 추가

--- apps/posts/models/__init__.py
+++ apps/posts/models/__init__.py
@@ -1,4 +1,5 @@
 from apps.posts.models.category import PostCategory
+from apps.posts.models.comment import PostComment, PostCommentTag
 from apps.posts.models.post import Post
 
-__all__ = ["PostCategory", "Post"]
+__all__ = ["PostCategory", "Post", "PostComment", "PostCommentTag"]
