4cc7361af66d00baedbf8ac8c45b70dd16ab05d8
jihyeongh21-svg <jihyeongh21@gmail.com>
2026-04-22 18:43:28 +0900

    💡 chore    : 오타 수정

--- apps/users/urls/auth_email.py
+++ apps/users/urls/auth_email.py
@@ -4,7 +4,7 @@ from apps.users.views.auth_email import EmailSendView, EmailVerificationView
 
 urlpatterns = [
     # 최종 경로: api/v1/accounts/verification/send-email/
-    path("verification/send-email/", EmailSendView.as_view(), name="send-email"),
+    path("verification/send-email", EmailSendView.as_view(), name="send-email"),
     # 최종 경로: api/v1/accounts/verification/verify-email/
-    path("verification/verify-email/", EmailVerificationView.as_view(), name="verify-email"),
+    path("verification/verify-email", EmailVerificationView.as_view(), name="verify-email"),
 ]
