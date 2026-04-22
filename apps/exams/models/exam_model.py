4e5a02f4c13e771fd50cf506c06ed940b9d54e1b
yimiro <s_icay@naver.com>
2026-04-22 15:09:08 +0900

    feat: 쪽지시험 목록, 생성 작성
    # Conflicts:
    #	apps/exams/models/exam_model.py

--- apps/exams/models/exam_model.py
+++ apps/exams/models/exam_model.py
@@ -5,9 +5,13 @@ from apps.posts.models.subject import Subject
 
 
 class Exam(TimeStampModel):
-    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name="exams")
     title = models.CharField(max_length=50)
     thumbnail_image_url = models.CharField(max_length=255, default="default_img_url")
 
     class Meta:
         db_table = "exams"
+        ordering = ["-created_at", "title"]
+
+    def __str__(self):
+        return self.title
