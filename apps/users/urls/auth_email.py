e8894e7a995b2a56b79dd69928cab216de81ce81
jihyeongh21-svg <jihyeongh21@gmail.com>
2026-04-22 17:12:51 +0900

    🎨 style    : 코드 포매팅 수정

--- apps/users/urls/auth_email.py
+++ apps/users/urls/auth_email.py
@@ -3,9 +3,8 @@ from django.urls import path
 from apps.users.views.auth_email import EmailSendView, EmailVerificationView
 
 urlpatterns = [
-# 최종 경로: api/v1/accounts/verification/send-email/
+    # 최종 경로: api/v1/accounts/verification/send-email/
     path("verification/send-email/", EmailSendView.as_view(), name="send-email"),
-
     # 최종 경로: api/v1/accounts/verification/verify-email/
     path("verification/verify-email/", EmailVerificationView.as_view(), name="verify-email"),
 ]
