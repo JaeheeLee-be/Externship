0e94b532f1ef6732d1473b0603c7225617add80d
kochanyeol <cksdufqqpu@gmail.com>
2026-04-23 10:43:43 +0900

    ♻️ refactor : URL 하드코딩 제거 및 reverse 적용

--- apps/exams/admin_urls.py
+++ apps/exams/admin_urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 
 from apps.exams.views.admin_exam_deployment_view import AdminExamDeploymentCreateView
 
-urlpatterns = [path("deployments/", AdminExamDeploymentCreateView.as_view())]
+urlpatterns = [path("deployments/", AdminExamDeploymentCreateView.as_view(), name="exam-deployment-create")]
