1d6600117c261cc33343758668617e4e27f8d633
yimiro <s_icay@naver.com>
2026-04-23 13:57:31 +0900

    fix: FK모델 생성에 맞춰 수정

--- apps/exams/models/exam_model.py
+++ apps/exams/models/exam_model.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 from apps.core.models import TimeStampModel
-from apps.posts.models.subject import Subject
+from apps.posts.models import Subject
 
 
 class Exam(TimeStampModel):
-    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name="exams")
-    title = models.CharField(max_length=50)
+    subject = models.ForeignKey(Subject,on_delete=models.CASCADE, related_name="exams")
+    title = models.CharField(max_length=50, unique=True)
     thumbnail_image_url = models.CharField(max_length=255, default="default_img_url")
 
     class Meta:
