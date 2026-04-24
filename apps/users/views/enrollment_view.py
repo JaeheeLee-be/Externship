f901b82eaa7084b4b5e2ecbeb41fb1861613326e
netrunner <netrunnerr25@gmail.com>
2026-04-24 14:16:19 +0900

    ✅ test: extend_schema 추가 후 테스트완료

--- apps/users/views/enrollment_view.py
+++ apps/users/views/enrollment_view.py
@@ -1,18 +1,29 @@
+from drf_spectacular.utils import OpenApiResponse, extend_schema
 from rest_framework import status
 from rest_framework.exceptions import ValidationError
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from apps.users.models import User
-from apps.users.serializers.enrollment import EnrollmentSerializer
-from apps.users.services.enrollment import create_enrollment
+from apps.users.serializers.enrollment_serializer import EnrollmentSerializer
+from apps.users.services.enrollment_service import create_enrollment
 
 
 class EnrollmentView(APIView):
     permission_classes = [IsAuthenticated]
 
+    @extend_schema(
+        summary="수강생 등록 신청 API",
+        description="로그인한 유저만 과정과 기수를 선택하여 수강생 등록 신청할 수 있습니다",
+        request=EnrollmentSerializer,
+        responses={
+            201: OpenApiResponse(description="수강생 등록 신청완료."),
+            400: OpenApiResponse(description="잘못된 요청"),
+            401: OpenApiResponse(description="인증 실패"),
+        },
+    )
     def post(self, request: Request) -> Response:
         user = request.user
         # mypy에서 IsAuthenticated 못 읽어서 추가함
