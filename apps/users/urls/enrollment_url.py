e5dc1a691c7a104284029b71b66e4bb9313a528d
netrunner <netrunnerr25@gmail.com>
2026-04-24 14:15:16 +0900

    ♻️ refactor:파일명 명시적으로 변경

--- apps/users/urls/enrollment_url.py
+++ apps/users/urls/enrollment_url.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from apps.users.views.enrollment import EnrollmentView
+from apps.users.views.enrollment_view import EnrollmentView
 
 urlpatterns = [
     path("enroll-student", EnrollmentView.as_view(), name="enroll-student"),
