8ccd3890f4feb5fc8cfbdf1f6fa5562dd2d71c43
yimiro <s_icay@naver.com>
2026-04-24 11:39:27 +0900

    fix: 예외처리 역할분리

--- apps/exams/exceptions/exam_exception.py
+++ apps/exams/exceptions/exam_exception.py
@@ -1,14 +1,6 @@
-from rest_framework import status
-from rest_framework.exceptions import APIException
+class ExamTitleConflict(Exception):
+    pass
 
 
-class ExamTitleConflict(APIException):
-    status_code = status.HTTP_409_CONFLICT
-    default_detail = "동일한 이름의 시험이 이미 존재합니다."
-    default_code = "exam_title_exists"
-
-
-class SubjectNotFound(APIException):
-    status_code = status.HTTP_404_NOT_FOUND
-    default_detail = "해당 과목 정보를 찾을 수 없습니다."
-    default_code = "subject_not_found"
+class SubjectNotFound(Exception):
+    pass
