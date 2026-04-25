45a1fbbb8b7ba49faddb9be254c57f3ad77ed901
yimiro <s_icay@naver.com>
2026-04-25 11:19:03 +0900

    feat: exams presigned-url 발급 api 작성
    # Conflicts:
    #	config/urls.py
    
    # Conflicts:
    #	apps/exams/admin_urls.py
    #	config/urls.py

--- apps/exams/admin_urls.py
+++ apps/exams/admin_urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from apps.exams.views.admin_exam_deployment_view import AdminExamDeploymentCreateView
+from apps.exams.views.exam_presigned_url_view import PresignedUrlView
 from apps.exams.views.admin_exam_view import ExamListCreateView
 
 urlpatterns = [
     path("deployments/", AdminExamDeploymentCreateView.as_view(), name="exam-deployment-create"),
+    path("presigned-url", PresignedUrlView.as_view(), name="presigned-url"),
     path("", ExamListCreateView.as_view(), name="exam-list"),
 ]
