cd9ccf5c3655b9984f2bf253a903d3eb83dd2e5c
gumba <gumba6740@gmail.com>
2026-04-26 14:11:23 +0900

    test: mock_aws에 관한 테스트 작성
    - @mock_aws 동작 테스트
    - 네트워크 차단 환경에서도 s3의 메서드 동작 확인

--- apps/core/tests/test_s3_urls.py
+++ apps/core/tests/test_s3_urls.py
@@ -1,3 +1,6 @@
+from unittest.mock import patch
+
+from botocore.exceptions import HTTPClientError
 from django.test import TestCase
 from moto import mock_aws
 
@@ -69,7 +72,6 @@ class TestS3Handler(TestCase):
         self.assertLessEqual(len(img_url), 255)
 
     # presigned_url이 잘 생성되는지
-    @mock_aws
     def test_presigned_url_for_upload(self) -> None:
         presigned_url = s3._presigned_url_for_upload(self.key, self.content_type)
         self.assertEqual(presigned_url.count("?"), 1)
@@ -81,9 +83,64 @@ class TestS3Handler(TestCase):
         self.assertIn("X-Amz-Expires=600", presigned_url)
 
     # s3.create_upload_urls()가 잘 작동하는지
-    @mock_aws
     def test_create_upload_urls(self) -> None:
         presigned_url, img_url = s3.create_upload_urls(self.file_name, self.path)
         self.assertTrue(presigned_url.startswith("https://"))
         self.assertTrue(img_url.startswith("https://"))
         self.assertNotEqual(presigned_url, img_url)
+
+
+class TestMock(TestCase):
+    """
+    테스트 요약:
+
+        - @mock_aws가 정상적으로 모킹을 하는지 테스트.
+        - s3.create_upload_urls()가 실제 aws 서버 호출 없이 동작하는 메서드인지 테스트
+    """
+
+    @mock_aws
+    def test_mock(self) -> None:
+        """
+        s3가 테스트 케이스 외부에서 생성된 인스턴스임에도
+        @mock_aws가 aws 서버와의 실제 접촉을 잘 차단하는지 테스트
+
+        결과:
+            @mock_aws 없었을 때:
+                'Buckets': [{'Name': 'oz-externship', ...}]
+
+            @mock_aws 적용했을 때:
+                'Buckets': []
+
+        결론: aws와의 접촉을 잘 차단함.
+        """
+
+        bucket_list = s3.s3.list_buckets()
+        self.assertEqual(bucket_list["Buckets"], [])
+
+    @patch("socket.socket", None)
+    def test_patch(self) -> None:
+        """
+        @patch가 네트워크 연결을 잘 차단하는지 테스트
+
+        결과: s3.s3.list_buckets() 실행 도중 HTTPClientError 발생
+
+        결론: 네트워크 차단 잘 작동함
+        """
+
+        with self.assertRaises(HTTPClientError):
+            s3.s3.list_buckets()
+
+    @patch("socket.socket", None)
+    def test_s3_create_upload_urls(self) -> None:
+        """
+        s3.create_upload_urls가 aws에 접촉하는 함수인지 아닌지를 테스트
+
+        결과: list_buckets()는 네트워크가 끊겼을 때 에러가 났지만,
+            create_upload_urls()는 에러가 나지 않고 잘 작동함
+
+        결론: s3.create_upload_urls는 로컬 계산만 하는 함수임.
+        """
+
+        presigned_url, img_url = s3.create_upload_urls("test_file.jpg", "test/")
+        self.assertTrue(presigned_url.startswith("https://"))
+        self.assertTrue(img_url.startswith("https://"))
