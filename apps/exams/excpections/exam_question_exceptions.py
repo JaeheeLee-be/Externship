69278281321a00c8e9392aefb8389bc95db509fa
gunung-kim <unge001@naver.com>
2026-04-27 16:17:07 +0900

    🐛 fix      : 쪽지시험 문제 생성 및 수정 피드백 수정_3

--- apps/exams/excpections/exam_question_exceptions.py
+++ apps/exams/excpections/exam_question_exceptions.py
@@ -1,44 +1,18 @@
-from rest_framework import status
-
-# from rest_framework.exceptions import APIException
-
-
-class ExamQuestionNotFound(Exception):
-    def __init__(self):
-        self.status_code = status.HTTP_404_NOT_FOUND
-        self.default_detail = "쪽지시험 문제 등록 권한이 없습니다."
+class ExamQuestionCreateNotFound(Exception):
+    def __init__(self, message: str = "쪽지시험 문제 등록 권한이 없습니다."):
+        super().__init__(message)
 
 
-class ExamQuestionValidationError(Exception):
-    def __init__(self):
-        self.status_code = status.HTTP_400_BAD_REQUEST
-        self.default_detail = "유효하지 않은 문제 등록 데이터입니다."
+class ExamQuestionCreateConflict(Exception):
+    def __init__(self, message: str = "해당 쪽지시험에 등록 가능한 문제 수 또는 총 배점을 초과했습니다."):
+        super().__init__(message)
 
 
-class ExamQuestionConflict(Exception):
-    def __init__(self):
-        self.status_code = status.HTTP_409_CONFLICT
-        self.default_detail = "해당 쪽지시험에 등록 가능한 문제 수 또는 총 배점을 초과했습니다."
+class ExamQuestionUpdateNotFound(Exception):
+    def __init__(self, message: str = "수정하려는 문제 정보를 찾을 수 없습니다."):
+        super().__init__(message)
 
 
-class ExamQuestionUnauthorized(Exception):
-    def __init__(self):
-        self.status_code = status.HTTP_401_UNAUTHORIZED
-        self.default_detail = "자격 인증 데이터가 제공되지 않았습니다."
-
-
-class ExamQuestionForbidden(Exception):
-    def __init__(self):
-        self.status_code = status.HTTP_403_FORBIDDEN
-        self.default_detail = "쪽지시험 문제 등록 권한이 없습니다."
-
-
-#
-# class ExamQuestionUnauthorized(APIException):
-#     status_code = status.HTTP_401_UNAUTHORIZED
-#     default_detail = "자격 인증 데이터가 제공되지 않았습니다."
-#
-#
-# class ExamQuestionCreateForbidden(APIException):
-#     status_code = status.HTTP_403_FORBIDDEN
-#     default_detail = "쪽지시험"
+class ExamQuestionUpdateConflict(Exception):
+    def __init__(self, message: str = "시험 문제 수 제한 또는 총 배점을 초과하여 문제를 수정할 수 없습니다."):
+        super().__init__(message)
