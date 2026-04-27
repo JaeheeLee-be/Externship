a620711b06fa744f443ad392103741e355c9d2ba
gunung-kim <unge001@naver.com>
2026-04-27 17:16:04 +0900

    ✨ feat     : 쪽지시험 문제 생성 및 수정 피드백 수정_3

--- config/urls.py
+++ config/urls.py
@@ -9,7 +9,7 @@ from drf_spectacular.views import (
 
 urlpatterns: list[URLPattern | URLResolver] = [
     path("api/v1/posts/", include("apps.posts.urls")),
-    path("api/v1/", include("apps.qna.urls.urls")),
+    path("api/v1/questions/", include("apps.qna.urls.urls")),
     path("api/v1/admin/qna/", include("apps.qna.urls.admin_urls")),
     path("api/v1/admin/exams/", include("apps.exams.admin_urls")),
     path("api/v1/accounts/", include("apps.users.urls", namespace="users")),
