4785266cd96dc459504e0a07d7214ecdfa756cb2
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-04-27 18:07:56 +0900

    feat:어드민 답변 삭제 API 기능 구현

--- apps/qna/urls/admin_urls.py
+++ apps/qna/urls/admin_urls.py
@@ -1,18 +1,14 @@
 from django.urls import path
 
+from apps.qna.views.admin_category_views import AdminCategoryCreateAPIView
 from apps.qna.views.admin_answer_view import AdminAnswerDeleteView
-from apps.qna.views.admin_category_views import (
-    AdminCategoryDestroyAPIView,
-    AdminCategoryListCreateAPIView,
-)
 
 urlpatterns = [
     # 어드민 카테고리
     path(
         "categories",
-        AdminCategoryListCreateAPIView.as_view(),
-        name="admin-category-list-create",
+        AdminCategoryCreateAPIView.as_view(),
+        name="admin-category-create",
     ),
-    path("categories/<int:category_id>", AdminCategoryDestroyAPIView.as_view(), name="admin-category-delete"),
-    path("answers/<int:answer_id>", AdminAnswerDeleteView.as_view(), name="admin-answer-delete"),
+    path("answers/<int:answer_id>",AdminAnswerDeleteView.as_view(),name="admin-answer-delete"),
 ]
