c2bb5f38345fc8f48443de76ad5f8042dacf3209
netrunner <netrunnerr25@gmail.com>
2026-04-27 14:07:38 +0900

    :💡 chore: urls 경로 수정
    # Conflicts:
    #	apps/users/urls/__init__.py
    #	config/urls.py
    
    # Conflicts:
    #	config/urls.py

--- apps/users/urls/enrollment_url.py
+++ apps/users/urls/enrollment_url.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from apps.users.views.enrollment_view import EnrollmentView
 
+app_name = "users"
+
 urlpatterns = [
     path("enroll-student", EnrollmentView.as_view(), name="enroll-student"),
 ]
