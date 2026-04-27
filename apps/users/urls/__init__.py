f3b3f33267bff158a2f6a4933b2ba9794ec77536
jihyeongh21-svg <jihyeongh21@gmail.com>
2026-04-27 15:45:41 +0900

    :# Commit Convention ♻️ refactor : url변경
    # Conflicts:
    #	config/urls.py
    
    # Conflicts:
    #	config/urls.py
    
    # Conflicts:
    #	apps/users/urls/__init__.py
    #	config/urls.py
    
    # Conflicts:
    #	config/urls.py
    
    # Conflicts:
    #	apps/users/urls/__init__.py

--- apps/users/urls/__init__.py
+++ apps/users/urls/__init__.py
@@ -3,8 +3,9 @@ from django.urls import include, path
 app_name = "users"
 
 urlpatterns = [
-    path("accounts/", include("apps.users.urls.account_url")),
+    path("", include("apps.users.urls.auth_email_url")),
     path("", include("apps.users.urls.social_url")),
     path("", include("apps.users.urls.enrollment_url")),
-    path("accounts/", include("apps.users.urls.profile_image_urls")),
+    path("", include("apps.users.urls.profile_image_urls")),
+path("", include("apps.users.urls.auth_sms_url")),
 ]
--- apps/users/urls/auth_email_url.py
+++ apps/users/urls/auth_email_url.py
@@ -2,9 +2,11 @@ from django.urls import include, path
 
 from apps.users.views.auth_email_view import EmailSendView, EmailVerificationView
 
+
 urlpatterns = [
     # 최종 경로: api/v1/accounts/verification/send-email
-    path("verification/send-email", EmailSendView.as_view(), name="send-email"),
+    path("accounts/verification/send-email", EmailSendView.as_view(), name="send-email"),
     # 최종 경로: api/v1/accounts/verification/verify-email
-    path("verification/verify-email", EmailVerificationView.as_view(), name="verify-email"),
+    path("accounts/verification/verify-email", EmailVerificationView.as_view(), name="verify-email"),
+    # 최종 경로: api/v1/accounts/verification/send-email
 ]
