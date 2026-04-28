8bf7c293e3e4b26bed95d390c1aad4d3b9372ebe
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-04-28 10:39:50 +0900

    test: test코드 작성

--- apps/qna/urls/admin_urls.py
+++ apps/qna/urls/admin_urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from apps.qna.views.admin_category_views import AdminCategoryCreateAPIView
 from apps.qna.views.admin_answer_view import AdminAnswerDeleteView
+from apps.qna.views.admin_category_views import AdminCategoryCreateAPIView
 
 urlpatterns = [
     # 어드민 카테고리
@@ -10,5 +10,5 @@ urlpatterns = [
         AdminCategoryCreateAPIView.as_view(),
         name="admin-category-create",
     ),
-    path("answers/<int:answer_id>",AdminAnswerDeleteView.as_view(),name="admin-answer-delete"),
+    path("answers/<int:answer_id>", AdminAnswerDeleteView.as_view(), name="admin-answer-delete"),
 ]
