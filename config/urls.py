5ba32adc8384ed97265615963ece534418161aca
netrunner <netrunnerr25@gmail.com>
2026-04-28 16:26:44 +0900

    💡 chore: 시리얼라이저, url 오타 수정

--- config/urls.py
+++ config/urls.py
@@ -12,7 +12,7 @@ urlpatterns: list[URLPattern | URLResolver] = [
     path("api/v1/", include("apps.qna.urls.urls")),
     path("api/v1/admin/qna/", include("apps.qna.urls.admin_urls")),
     path("api/v1/admin/exams/", include("apps.exams.admin_urls")),
-    path("api/vi/accounts", include("apps.users.urls", namespace="users")),
+    path("api/v1/accounts/", include("apps.users.urls", namespace="users")),
 ]
 
 if settings.DEBUG:
