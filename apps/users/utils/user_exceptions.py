3854271ce1eb25f82c4887fc8c0c578ae1737c8d
netrunner <netrunnerr25@gmail.com>
2026-04-29 14:24:47 +0900

    ✨ feat: 커스텀에러 파일 추가

---
+++ apps/users/utils/user_exceptions.py
@@ -0,0 +1,13 @@
+from rest_framework.exceptions import APIException
+
+
+class NotAuthenticatedError(APIException):
+    status_code = 401
+    default_detail = "자격 인증 데이터가 제공되지 않았습니다."
+    default_code = "not_authenticated"
+
+
+class DuplicateNicknameError(APIException):
+    status_code = 409
+    default_detail = "중복된 닉네임이 존재합니다."
+    default_code = "duplicate_nickname"
