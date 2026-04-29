07c6a2bc7e1f6401363f5d8f4bdfd64a38164fda
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-04-29 17:32:20 +0900

    fix:에러 메세지 수정

--- apps/qna/schemas/answer_schemas.py
+++ apps/qna/schemas/answer_schemas.py
@@ -14,7 +14,8 @@ answer_accept_schema = extend_schema(
         200: AnswerAcceptResponseSerializer,
         401: OpenApiResponse(description="로그인한 사용자만 채택할 수 있습니다."),
         403: OpenApiResponse(description="본인의 질문에 대한 답변만 채택할 수 있습니다."),
-        404: OpenApiResponse(description="해당 갑변을 찾을 수 없습니다."),
+        404: OpenApiResponse(description="해당 질문 또는 답변을 찾을 수 없습니다."),
+        409: OpenApiResponse(description="이미 채택된 답변이 존재합니다.")
     },
 )
 
@@ -27,7 +28,7 @@ answer_create_schema = extend_schema(
         201: AnswerResponseSerializer,
         400: OpenApiResponse(description="잘못된 요청 입니다."),
         401: OpenApiResponse(description="로그인한 사용자만 채택할 수 있습니다."),
-        403: OpenApiResponse(description="관리자가 아닙니다."),
-        404: OpenApiResponse(description="찾을 수 없는 질문 입니다."),
+        403: OpenApiResponse(description="답변 작성 권한이 없습니다."),
+        404: OpenApiResponse(description="해당 질문을 찾을 수 없습니다."),
     },
 )
--- apps/qna/services/answer_services.py
+++ apps/qna/services/answer_services.py
@@ -47,7 +47,7 @@ class AnswerAcceptService:
             if answer.question.author_id != user.id:
                 raise PermissionDeniedException("본인이 작성한 질문의 답변만 채택할 수 있습니다.")
             if Answer.objects.filter(question_id=answer.question_id, is_adopted=True).exists():
-                raise ConflictException("이미 채택된 답변이 존재합니다.")
+                raise ConflictException()
             answer.is_adopted = True
             answer.save()
         return answer
