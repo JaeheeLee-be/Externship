6de9369eaf6cae8824d87922663fcb7dfa1ec432
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-04-29 14:19:22 +0900

    test: test 코드 수정

--- apps/qna/tests/test_answers/test_answers_view.py
+++ apps/qna/tests/test_answers/test_answers_view.py
@@ -91,6 +91,12 @@ class AnswerAcceptViewTestCase(BaseTestCase):
     답변 채택에 대한 test code
     """
 
+    @classmethod
+    def setUpTestData(cls) -> None:
+        super().setUpTestData()
+        cls.user.role = "STUDENT"
+        cls.user.save()
+
     def setUp(self) -> None:
         """작성자와 다른 유저가 답변을 달 수 있어 더미 데이터 생성"""
         self.client = APIClient()
