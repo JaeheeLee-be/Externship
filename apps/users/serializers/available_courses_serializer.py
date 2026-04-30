468db52da5fe777b4cf36866dd13ab99d176347c
netrunner <netrunnerr25@gmail.com>
2026-04-30 16:31:37 +0900

    ✨ feat: 수강신청 가능한 기수 조회 기능 추가
    # Conflicts:
    #	apps/users/urls/__init__.py

---
+++ apps/users/serializers/available_courses_serializer.py
@@ -0,0 +1,31 @@
+from typing import Any
+
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+
+from apps.courses.models.cohort import Cohort
+from apps.posts.models.course import Course
+
+
+# 안쪽 시리얼라이저
+class CohortInfoSerializer(serializers.ModelSerializer[Cohort]):
+    class Meta:
+        model = Cohort
+        fields = ["id", "number", "start_date", "end_date", "status"]
+        read_only_fields = fields
+
+
+class CourseInfoSerializer(serializers.ModelSerializer[Course]):
+    class Meta:
+        model = Course
+        fields = ["id", "name"]
+        read_only_fields = fields
+
+
+# 메인 시리얼라이저(위에 두개 묶어주기)
+class AvailableCoursesSerializer(serializers.Serializer[Any]):
+    course = CourseInfoSerializer(read_only=True)
+    cohort = CohortInfoSerializer(source="*", read_only=True)
+    # source='*'의 역할:
+    # 1. 특정 필드가 아닌 '객체(instance) 전체'를 전달할때 사용
+    # 2. cohort가 모델 필드명에 없어서 소스를 지정하는 용도
--- apps/users/urls/user_url.py
+++ apps/users/urls/user_url.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
+from apps.users.views.available_courses_view import AvailableCoursesView
 from apps.users.views.enrollment_view import EnrollmentView
 from apps.users.views.user_info_view import UserInfoView
 
 urlpatterns = [
     path("enroll-student", EnrollmentView.as_view(), name="enroll-student"),
     path("me", UserInfoView.as_view(), name="me"),
+    path("available-courses", AvailableCoursesView.as_view(), name="available-courses"),
 ]
