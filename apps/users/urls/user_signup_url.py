a2e1ebdbe100c8b19e6e3bf3a1a27a90961a889b
jihyeongh21-svg <jihyeongh21@gmail.com>
2026-04-30 18:52:29 +0900

    💡 chore    : 기능 추가 없이 코드 수정

--- apps/users/urls/user_signup_url.py
+++ apps/users/urls/user_signup_url.py
@@ -3,8 +3,10 @@ from django.urls import path
 from apps.users.views.available_courses_view import AvailableCoursesView
 from apps.users.views.enrollment_view import EnrollmentView
 from apps.users.views.user_info_view import UserInfoView
+from apps.users.views.user_signup_view import SignupView
 
 urlpatterns = [
+    path("signup", SignupView.as_view(), name="signup"),
     path("enroll-student", EnrollmentView.as_view(), name="enroll-student"),
     path("me", UserInfoView.as_view(), name="me"),
     path("available-courses", AvailableCoursesView.as_view(), name="available-courses"),
