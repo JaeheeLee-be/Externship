78ce41c2b612bc459b7f449de7f5d2c13896d867
RyuHawon <hfixjsj49@gmail.com>
2026-04-30 10:58:40 +0900

    충돌수정

--- apps/exams/serializers/admin_exam_deployment_serializer.py
+++ apps/exams/serializers/admin_exam_deployment_serializer.py
@@ -3,10 +3,10 @@ from typing import Any
 from django.utils import timezone
 from rest_framework import serializers
 
-from apps.exams.models.exam_model import Exam
 from apps.courses.models.cohort import Cohort
-from apps.posts.models.course import Course
 from apps.courses.models.subject import Subject
+from apps.exams.models.exam_model import Exam
+from apps.posts.models.course import Course
 
 
 class SubjectSummarySerializer(serializers.ModelSerializer[Subject]):
