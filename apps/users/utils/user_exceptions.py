b5e48178a32c203675dbe574282e35eacd87f4dc
jihyeongh21-svg <jihyeongh21@gmail.com>
2026-05-04 14:03:15 +0900

    # 💡  chore: user_exceptions 수정

--- apps/users/utils/user_exceptions.py
+++ apps/users/utils/user_exceptions.py
@@ -1,18 +1,13 @@
-from rest_framework.exceptions import APIException
+class NotAuthenticatedError(Exception):
+    def __init__(self) -> None:
+        super().__init__("자격 인증 데이터가 제공되지 않았습니다.")
 
 
-class NotAuthenticatedError(APIException):
-    status_code = 401
-    default_detail = "자격 인증 데이터가 제공되지 않았습니다."
-    default_code = "not_authenticated"
-
-
-class DuplicateNicknameError(APIException):
-    status_code = 409
-    default_detail = "중복된 닉네임이 존재합니다."
-    default_code = "duplicate_nickname"
+class DuplicateNicknameError(Exception):
+    def __init__(self) -> None:
+        super().__init__("중복된 닉네임이 존재합니다.")
 
 
 class ConflictError(Exception):
-    default_detail = "이미 중복된 회원 가입 내역이 존재 합니다."
-    default_code = "conflict"
+    def __init__(self, message="이미 중복된 회원 가입 내역이 존재 합니다.") -> None:
+        super().__init__(message)
