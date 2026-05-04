e8dfe13569132928582788e4a58a5f4012c87687
gumba <gumba6740@gmail.com>
2026-05-04 22:09:48 +0900

    feat: 초기응답 시리얼라이저

--- apps/qna/serializers/chatbot_serializers.py
+++ apps/qna/serializers/chatbot_serializers.py
@@ -1,7 +1,9 @@
+from typing import Any
+
 from rest_framework import serializers
 
 
-class InitialAIAnswerSerializer(serializers.Serializer):
+class InitialAIAnswerSerializer(serializers.Serializer[Any]):
     question_id = serializers.IntegerField()
     output = serializers.CharField(source="answer")
     using_model = serializers.CharField()
