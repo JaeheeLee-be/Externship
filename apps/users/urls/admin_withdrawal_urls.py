f8c94638a076b3679daf6a896beb1b704fc53abf
jongwonkim987 <jongwonkim987@gmail.com>
2026-05-06 18:26:34 +0900

    fix: 관리자 탈퇴 목록 명세 반영

--- apps/users/urls/admin_withdrawal_urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from apps.users.views.admin_withdrawal_view import AdminWithdrawalListView
-
-urlpatterns = [
-    path("withdrawals", AdminWithdrawalListView.as_view(), name="admin-withdrawal-list"),
-]
