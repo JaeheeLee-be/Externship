5d7c728c033c1b90c4a116268b344aa409349fd1
netrunner <netrunnerr25@gmail.com>
2026-05-06 17:09:34 +0900

    ♻️ refactor : 리베이스

--- apps/users/utils/user_exceptions.py
+++ apps/users/utils/user_exceptions.py
@@ -2,19 +2,9 @@ from datetime import date
 from typing import Optional
 
 
-class NotAuthenticatedError(Exception):
-    def __init__(self) -> None:
-        super().__init__("자격 인증 데이터가 제공되지 않았습니다.")
-
-
 class DuplicateNicknameError(Exception):
     def __init__(self) -> None:
         super().__init__("중복된 닉네임이 존재합니다.")
-
-
-class ConflictError(Exception):
-    def __init__(self, message: str = "이미 중복된 회원 가입 내역이 존재 합니다.") -> None:
-        super().__init__(message)
 
 
 class InvalidLoginError(Exception):
