97c1cdad1d267b252e9c63c80739bc70b7640ed0
gumba <gumba6740@gmail.com>
2026-05-06 09:35:54 +0900

    style: 코드포매팅

--- apps/qna/exceptions.py
+++ apps/qna/exceptions.py
@@ -22,7 +22,6 @@ class ConflictException(BaseCustomException):
     default_message = "이미 채택된 답변이 존재합니다."
 
 
-
 class ParentNotFoundException(NotFoundException):
     default_message = "부모 카테고리를 찾을 수 없습니다."
 
