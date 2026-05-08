d51c1be7b08ce835ccf56bd77c4c725499d04e83
jihyeongh21-svg <jihyeongh21@gmail.com>
2026-05-08 15:21:31 +0900

    💡 chore: mypy 타입 어노테이션 수정

--- apps/users/serializers/find_email_serializer.py
+++ apps/users/serializers/find_email_serializer.py
@@ -8,5 +8,5 @@ class FindEmailSerializer(serializers.Serializer[Any]):
     name = serializers.CharField(required=True, error_messages={"required": "이 필드는 필수 항목입니다."})
 
 
-class FindEmailResponseSerializer(serializers.Serializer):
+class FindEmailResponseSerializer(serializers.Serializer[Any]):
     email = serializers.CharField()
