de345a1cd8b9330b8a21c37453fe2b551ba2103c
em <ererjomk5612@gmail.com>
2026-05-08 14:59:23 +0900

    🐛 fix      : 명세서 요청 URL에 맞게 수정

--- apps/users/urls/admin_urls.py
+++ apps/users/urls/admin_urls.py
@@ -10,7 +10,7 @@ from apps.users.views.enrollment_accept_view import AdminStudentEnrollmentAccept
 urlpatterns = [
     path("accounts", AdminAccountListView.as_view(), name="admin-account-list"),
     path("accounts/<int:account_id>", AdminAccountDetailView.as_view(), name="admin-account-detail"),
-    path("accounts/<int:account_id>/update", AdminAccountUpdateView.as_view(), name="admin-account-update"),
+    path("accounts/<int:account_id>", AdminAccountUpdateView.as_view(), name="admin-account-update"),
     path("student-enrollments/accept", AdminStudentEnrollmentAcceptView.as_view(), name="admin-student-enrollment-accept"),
     path("accounts/<int:account_id>/role", AdminPermissionView.as_view(), name="admin_permission"),
 
