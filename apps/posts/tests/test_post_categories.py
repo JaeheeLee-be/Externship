f051abf7ca44891cce32820ae41ce9d48c9ec4f2
RyuHawon <hfixjsj49@gmail.com>
2026-05-08 14:34:00 +0900

    게시글 카테고리 조회 테스트코드

---
+++ apps/posts/tests/test_post_categories.py
@@ -0,0 +1,35 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient, APITestCase
+
+from apps.posts.models import PostCategory
+
+
+class PostCategoryListTest(APITestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+        self.url = reverse("post_category_list")
+
+    def test_list_categories_returns_200(self) -> None:
+        PostCategory.objects.create(name="자유게시판")
+
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_list_categories_response_shape(self) -> None:
+        category = PostCategory.objects.create(name="자유게시판")
+
+        response = self.client.get(self.url)
+
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0], {"id": category.id, "name": "자유게시판"})
+
+    def test_list_categories_returns_all(self) -> None:
+        names = ["자유게시판", "코스게시판", "기수게시판"]
+        for name in names:
+            PostCategory.objects.create(name=name)
+
+        response = self.client.get(self.url)
+
+        self.assertEqual(len(response.data), 3)
