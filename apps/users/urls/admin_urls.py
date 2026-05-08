f4910d438558fb56d109e35f2406632165adecfc
em <ererjomk5612@gmail.com>
2026-05-08 18:51:11 +0900

    🎨 style    : 코드 포매팅 수정

--- apps/users/urls/admin_urls.py
+++ apps/users/urls/admin_urls.py
@@ -8,6 +8,8 @@ from apps.users.views.enrollment_accept_view import AdminStudentEnrollmentAccept
 urlpatterns = [
     path("accounts", AdminAccountListView.as_view(), name="admin-account-list"),
     path("accounts/<int:account_id>", AdminAccountView.as_view(), name="admin-account-detail"),
-    path("student-enrollments/accept", AdminStudentEnrollmentAcceptView.as_view(), name="admin-student-enrollment-accept"),
+    path(
+        "student-enrollments/accept", AdminStudentEnrollmentAcceptView.as_view(), name="admin-student-enrollment-accept"
+    ),
     path("accounts/<int:account_id>/role", AdminPermissionView.as_view(), name="admin_permission"),
 ]
