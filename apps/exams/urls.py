a13ad6d82529a25c70d693d021bb36060ec3a4c1
kochanyeol <cksdufqqpu@gmail.com>
2026-05-08 17:43:52 +0900

    🐛 fix : rebase 충돌로 누락된 url 복구

--- apps/exams/urls.py
+++ apps/exams/urls.py
@@ -6,8 +6,10 @@ from apps.exams.views.exam_deployment_view import (
     ExamDeploymentListView,
     ExamDeploymentStatusView,
 )
+from apps.exams.views.user_exam_submission_view import UserExamSubmissionGetView
 
 urlpatterns = [
+    path("submissions/<int:submission_id>", UserExamSubmissionGetView.as_view(), name="user-exam-submission-get"),
     path("deployments/", ExamDeploymentListView.as_view(), name="deployment"),
     path("deployments/<int:deployment_id>/check-code/", ExamDeploymentCheckCodeView.as_view(), name="deployment-code"),
     path("deployments/<int:deployment_id>/", ExamDeploymentDetailView.as_view(), name="deployment-detail"),
