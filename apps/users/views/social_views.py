b81478ddc032eb57b3424b608e5f68011e36ab73
inu9431 <>
2026-05-12 13:28:04 +0900

    social_refresh 설정 수정

--- apps/users/views/social_views.py
+++ apps/users/views/social_views.py
@@ -22,8 +22,8 @@ def set_auth_cookies(response: Any, refresh: str) -> None:
         max_age=60 * 60 * 24 * 4,  # 4일
         domain=getattr(settings, "COOKIE_DOMAIN", None),
         httponly=True,
-        # secure=getattr(settings, "COOKIE_SECURE", True),
-        samesite=getattr(settings, "COOKIE_SAMESITE", "Lax"),
+        secure=True,
+        samesite="None",
         path="/",
     )
 
