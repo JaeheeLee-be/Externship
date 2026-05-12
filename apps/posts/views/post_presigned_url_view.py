578132df22004a59bcd20140197dd865303ffa86
inu9431 <>
2026-05-12 11:42:27 +0900

    urls 수정

--- apps/posts/views/post_presigned_url_view.py
+++ apps/posts/views/post_presigned_url_view.py
@@ -18,5 +18,5 @@ class PostPresignedUrlView(PresignedUrlView):
     permission_classes = [IsAuthenticated]
     path = "uploads/images/posts/"
 
-    # def post(self, request, *args, **kwargs):
-    #     return super().post(request, *args, **kwargs)
+# def post(self, request, *args, **kwargs):
+#     return super().post(request, *args, **kwargs)
