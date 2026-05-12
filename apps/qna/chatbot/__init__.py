320f704eae19c211b309eb3e9543d83fb5facc32
gumba <gumba6740@gmail.com>
2026-05-12 11:39:58 +0900

    feat: cs 챗봇 프롬프트, redis의 CS 키

--- apps/qna/chatbot/__init__.py
+++ apps/qna/chatbot/__init__.py
@@ -1,9 +1,11 @@
 from .factories.payload import GroqPayloadFactory
 from .llm_models import GROQ_MODEL
+from .prompts.cs_prompt import CS_PROMPT
 from .prompts.qna_prompt import QNA_PROMPT
 
 __all__ = [
     "GroqPayloadFactory",
     "GROQ_MODEL",
     "QNA_PROMPT",
+    "CS_PROMPT",
 ]
