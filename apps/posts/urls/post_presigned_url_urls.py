96cc018c9ea519d10241f78127435e43aec9e15e
inu9431 <>
2026-05-13 13:46:16 +0900

    ♻️ refactor : urls 수정
    # Conflicts:
    #	apps/exams/admin_urls.py

--- apps/posts/urls/post_presigned_url_urls.py
+++ apps/posts/urls/post_presigned_url_urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from apps.posts.views.post_presigned_url_view import PostPresignedUrlView
 
 urlpatterns = [
-    path("presigned-url/", PostPresignedUrlView.as_view(), name="presigned_url"),
+    path("presigned-url", PostPresignedUrlView.as_view(), name="presigned_url"),
 ]
