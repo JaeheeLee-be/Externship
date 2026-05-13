0cc4bc94331eafc913eef6ae5f7f0b3f0991a088
inu9431 <>
2026-05-13 21:28:29 +0900

    ♻️ refactor : 마이그레이션 꼬인거 수정

---
+++ apps/users/migrations/0013_merge_20260513.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("users", "0012_alter_user_profile_img_url"),
+        ("users", "0012_alter_withdrawal_reason"),
+    ]
+
+    operations = []
