0c05ad29f551eabee311b5ba6130d1e3b20b644a
inu9431 <>
2026-05-13 15:13:37 +0900

    ♻️ refactor : urls 수정

--- apps/exams/admin_urls.py
+++ apps/exams/admin_urls.py
@@ -5,6 +5,7 @@ from apps.core.presigned_url.views import PresignedUrlView
 from apps.core.utils.permissions import IsRoleAdminUser
 from apps.exams.views.admin_exam_deployment_view import (
     AdminExamDeploymentDetailView,
+    AdminExamDeploymentStatusView,
     AdminExamDeploymentView,
 )
 from apps.exams.views.admin_exam_question_view import (
@@ -23,6 +24,11 @@ class ExamImageUploadView(PresignedUrlView):
 urlpatterns = [
     path("deployments", AdminExamDeploymentView.as_view(), name="exam-deployment"),
     path("deployments/<str:deployment_id>", AdminExamDeploymentDetailView.as_view(), name="exam-deployment-detail"),
+    path(
+        "deployments/<str:deployment_id>/status",
+        AdminExamDeploymentStatusView.as_view(),
+        name="exam-deployment-status",
+    ),
     path("presigned-url", ExamImageUploadView.as_view(), name="presigned-url"),
     path("<int:exam_id>/questions", AdminQuestionCreateView.as_view(), name="exam-question-create"),
     path("questions/<int:question_id>", AdminQuestionUpdateDeleteView.as_view(), name="exam-question-update-delete"),
